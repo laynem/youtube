3514afcef006efe839f12237266fe1562ca56a1b
Layne <lmatsusaki@gmail.com>
2024-11-25 10:27:40 -1000

    changes

---
+++ login.js
@@ -0,0 +1,37 @@
+import { 
+    getAuth, 
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDG2yH066S224ShC8PN27uv0eQYfNPwMa4",
+    authDomain: "fir-b6113.firebaseapp.com",
+    databaseURL: "https://fir-b6113-default-rtdb.firebaseio.com",
+    projectId: "fir-b6113",
+    storageBucket: "fir-b6113.firebasestorage.app",
+    messagingSenderId: "975424791293",
+    appId: "1:975424791293:web:51703a22f18a8845c91494",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+const user = auth.currentUser;
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        return uid;
+    } else {
+        window.location.href("index.html");
+    }   
+})
